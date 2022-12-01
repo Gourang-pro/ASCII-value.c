1fa7ec98f510def0ef220c931ab07b5c26a74698
Gourang-pro <117590608+Gourang-pro@users.noreply.github.com>
2022-12-01 17:05:17 +0530

    Add files via upload

---
+++ ASCII value.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{       
+
+    char ch;
+    printf("ENTER ANY CHARACTER : ");
+    scanf("%c",&ch);
+
+    printf("%d",ch);
+    
+
+
+ return 0;   
+}
